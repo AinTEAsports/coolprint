58a214f22d41203e3481bbf0afdaab878ebafa56
AinTEAsports <benkirat.khalil@gmail.com>
2022-07-04 16:48:13 +0200

    moduls/utils.py : added muliline string support, now you can coolprint some ascii art

--- modules/utils.py
+++ modules/utils.py
@@ -27,26 +27,29 @@ def cool_print(text : str, wait_time : float = 0.01, color : str = "white") -> N
         CharNotExistingError: if the word contains one or more letter that are not present in 'ALPHABET' constant
     """
 
-    for char in text:
-        if not char in ALPHABET:
-            raise CharNotSupportedError(f"one of the char you put in parameter 'word' ('{char}') is not supported, but you can add it in 'ALPHABET' variable in '/modules/utils.py'")
+    # for char in text:
+        # if not char in ALPHABET:
+            # raise CharNotSupportedError("one of the char you put in parameter 'word' ('{}') is not supported, but you can add it in 'ALPHABET' variable in '/modules/utils.py'".format(char))
 
 
     to_print = ""
     random_choosed = ''
 
-    for char in text:
-        while not random_choosed == char:
-            random_choosed = random.choice(ALPHABET)
+    for line in text.split('\n'):
+        for char in line:
+            while not random_choosed == char:
+                random_choosed = random.choice(ALPHABET)
 
-            colored_text = termcolor.colored(f"{to_print}{random_choosed}\r", color)
-            print(colored_text, end='')
-            
-            time.sleep(wait_time)
+                colored_text = termcolor.colored(f"{to_print}{random_choosed}\r", color)
+                print(colored_text, end='')
 
-        to_print += random_choosed
+                time.sleep(wait_time)
+
+            to_print += random_choosed
+
+        to_print = ""
+        print()
 
     colored_text = termcolor.colored(to_print, color)
     print(colored_text)
-
 
